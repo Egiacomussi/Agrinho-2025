2416928534d7d671ddd51f5ebabc6cba747839c7
Egiacomussi <egiacomussi@gmail.com>
2025-05-30 08:57:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+<script>
+    let index = 0;
+    const images = document.querySelectorAll(".carousel-inner img");
+    const totalImages = images.length;
+
+    function showImage(index) {
+        images.forEach((img, i) => {
+            img.style.display = i === index ? "block" : "none";
+            img.setAttribute("aria-hidden", i === index ? "false" : "true");
+        });
+    }
+
+    document.querySelector(".prev").addEventListener("click", function() {
+        index = index > 0 ? index - 1 : totalImages - 1;
+        showImage(index);
+    });
+
+    document.querySelector(".next").addEventListener("click", function() {
+        index = index < totalImages - 1 ? index + 1 : 0;
+        showImage(index);
+    });
+
+    showImage(index); // Exibir a imagem inicial corretamente
+</script>
 
